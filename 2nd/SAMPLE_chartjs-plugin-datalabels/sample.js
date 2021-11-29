f055c8cc92baf18ab93477b686e1bbf1b7cc5eee
textcunma <78415309+textcunma@users.noreply.github.com>
2021-11-30 06:34:38 +0900

    add(sample.html,sample.jss): add new radar graph function
    add new function :paint radar graph with Chart.js

--- 2nd/SAMPLE_chartjs-plugin-datalabels/sample.js
+++ 2nd/SAMPLE_chartjs-plugin-datalabels/sample.js
@@ -1,4 +1,5 @@
 const Btn = document.getElementById("Btn");
+const Btn2 = document.getElementById("Btn2");
 let myChart = null; 
 
 Btn.addEventListener('click', e => {
@@ -71,3 +72,77 @@ Btn.addEventListener('click', e => {
         }
     });
 });
+
+Btn2.addEventListener('click', e => {
+    if (myChart) {
+        myChart.destroy();
+    }
+
+    const ctx = document.getElementById('mychart');
+    myChart = new Chart(ctx, {
+        type: 'radar', 
+        data: {
+            labels: ['C++', 'Java', 'Python','HTML/CSS','JavaScript'],  
+            datasets: [{
+                label: "", 
+                data: [1,2,4,3,2],
+                borderColor: "red",
+                backgroundColor:"red",
+                pointStyle: "circle",
+                pointRadius: 20,
+            }],
+        },
+        plugins: [ChartDataLabels], 
+        options: {
+            responsive: true,
+            fill: false,
+            elements: {
+                line: {
+                  borderWidth: 5
+                }
+            },
+            scale:{
+                r:{
+                    min: 0,
+                    max: 5
+                },
+                ticks: {
+                    stepSize: 1
+                }
+            },
+            scales: {
+                r: {
+                    pointLabels: {
+                      font: {
+                        size: 20
+                      }
+                    }
+                }
+            },     
+            events: [],    
+            animation: {
+                delay:100,
+                duration: 2000,
+            },
+            plugins: {
+                legend: {
+                    display: false
+                },
+                datalabels: {
+                    backgroundColor: function(context) {
+                      return context.dataset.borderColor;
+                    },
+                    color: 'white',
+                    font: {
+                      weight: 'bold',
+                      size: 20,
+                    }
+                }
+            }
+            
+            
+        }
+    });
+    
+});
+
