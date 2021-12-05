e9fac65ba74d47a8ae53b7311453e442a4011e3b
textcunma <78415309+textcunma@users.noreply.github.com>
2021-12-05 21:03:20 +0900

    Update(lesson 3 files): non rough

---
+++ 3rd/References.md
@@ -0,0 +1,36 @@
+# 参考にしたサイト一覧
+
+### Tailwind CSS
+[利用者爆増中 初めてでもわかるTailwind CSS入門 基礎編](https://reffect.co.jp/html/tailwindcss-for-beginners)
+<br>
+
+[Tailwind CSSを使用したWebサイトのスタイル方法](https://www.twilio.com/blog/style-your-website-with-tailwind-css-jp)
+<br>
+
+[tailwind css製loading animation samples](https://zenn.dev/tacrew/articles/ddf073e841165f)
+
+### スクロールした際のアニメーション表現
+[ScrollReveal.jsを使ってスクロールしたらふわっと表示される動きを作ってみよう](https://liginc.co.jp/500530)
+
+### 非同期処理の基礎
+[【JavaScript】非同期処理 まとめ](https://zenn.dev/tentel/articles/8146043d1101b5ea873d)
+<br>
+
+[【JS】setTimeoutを用いた、非同期処理入門](https://qiita.com/Naoto9282/items/af0a38cc916176cdd50f)
+<br>
+
+[Promise, async/await](https://ja.javascript.info/async)
+
+
+### barba.js V2
+[Barba.js V2 Tutorial For Total Beginners](https://waelyasmina.com/barba-js-v2-tutorial-for-total-beginners/)
+<br>
+
+[How To Make Page Transitions in HTML – Barba.js and GSAP3 Tutorial](https://ihatetomatoes.net/how-to-make-page-transitions-in-html/)
+<br>
+
+[Barba.jsによる画面遷移を使う](https://qiita.com/NomuraS/items/29b72c9b12993154f91c)
+<br>
+
+[Barba.js v2でサイトを簡単に非同期遷移させる](https://www.willstyle.co.jp/blog/3055/)
+<br>
---
+++ 3rd/simple_barba/simple.js
@@ -0,0 +1,95 @@
+// Copyright (c) HTML5 Boilerplate
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+// of the Software, and to permit persons to whom the Software is furnished to do
+// so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+
+function init(){
+    
+    const loader = document.querySelector('.loader');
+
+    gsap.set(loader, {
+        x: 0,
+        y: 450,
+        scaleX: 0,
+        scaleY: 1,
+        rotation: 0, 
+        xPercent: 0,
+        yPercent: -90, 
+        transformOrigin: 'left center', 
+        autoAlpha: 1
+    });
+
+    function loaderIn() {
+        return gsap.fromTo(loader, 
+            {
+                rotation: 0,
+                scaleX: 0,
+                xPercent: 0
+            },
+            { 
+                duration: 0.8,
+                xPercent: 0,
+                scaleX: 1, 
+                rotation: 0,
+                ease: 'Power4.inOut', 
+                transformOrigin: 'left center'      //左から右に
+            });
+    }
+
+    function loaderAway() {
+        return gsap.to(loader, { 
+            duration: 0.8, 
+            scaleX: 0,
+            xPercent: 0, 
+            rotation: 0, 
+            transformOrigin: 'right center', 
+            ease: 'Power4.inOut'
+        });
+    }
+
+    //ローディングしたら必ずページトップへ
+    barba.hooks.enter(() => {
+        window.scrollTo(0, 0);
+    });
+
+    barba.init({
+        transitions: [{
+            //現在のページを離れる時
+            async leave() {
+                await loaderIn();
+        
+            },
+            //次のページを表示する時
+            enter() {
+                loaderAway();
+            }
+        }]
+    })
+
+}
+
+window.addEventListener('load', function(){
+    init();
+});
+
+// async :非同期関数
+// await :非同期処理を待つ
+
+
+// barba.jsだけではアニメーションできないので、[gsap]を使用する
